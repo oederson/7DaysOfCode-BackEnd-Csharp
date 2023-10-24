da9b162df9abc02f2ad0cd2bdd7b1305f399a553
oederson <eder.hu@live.com.pt>
2023-10-24 20:35:24 -0300

    Day 1 - concluido

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using ConsoleApp.Models;
+using System.Text.Json;
+
+Pokemon pokemon1 = await BuscaPokemon("kakuna");
+pokemon1.ExibirInformacoesPokemon();
+
+Pokemon pokemon2 = await BuscaPokemon("wartortle");
+pokemon2.ExibirInformacoesPokemon();
+
+Pokemon pokemon3 = await BuscaPokemon("charmander");
+pokemon3.ExibirInformacoesPokemon();
+
+Console.ReadKey();
+
+
+static async Task<Pokemon> BuscaPokemon(string nome)
+{
+    using (HttpClient httpClient = new HttpClient())
+    {
+        try
+        {
+            string resposta = await httpClient.GetStringAsync($"https://pokeapi.co/api/v2/pokemon/{nome}");
+            var pokemon = JsonSerializer.Deserialize<Pokemon>(resposta);
+            return pokemon;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+                return null;
+        }
+    }
+}
+            
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# 7DaysOfCode-BackEnd-Csharp
-Desafio 7 days of code backend C#
+# 7 Days Of Code BackEnd C#
+## Day 1 : 
+* Criar o código C# que executará uma requisição HTTP do tipo GET.
+* Executar a requisição e pegar a resposta (o JSON)
+* Imprimir o resultado no console
+## Solução:
+ Criei a função assíncrona BuscaPokemon, essa função faz uma solicitação HTTP para a API da PokeAPI para obter informações sobre um Pokémon com base em um nome fornecido. Ela utiliza o nome para construir a URL da solicitação, faz a solicitação e, em seguida, desserializa a resposta JSON em um objeto da classe Pokemon. Se a solicitação for bem-sucedida, o objeto Pokemon é retornado; caso contrário, se ocorrer uma exceção, a função retorna null.Criei o método ExibirInformacoesPokemon para imprimir as informções do Pokémon conforme pedido pelo desafio.
